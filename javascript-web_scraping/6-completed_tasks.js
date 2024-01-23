877e4513f39bb9369df284a25ce6071caae4c8e0
AaronEGH16 <aezegonz@gmail.com>
2024-01-23 20:30:40 -0300

    count all completed tasks in json URL)

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require("request");
+const args = process.argv;
+
+request.get(args[2], '', (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    let dict = {};
+    const users = JSON.parse(body);
+
+    for (const user of users) {
+      if (user.completed) {
+        if (dict[user.userId]) {
+          dict[user.userId] += 1;
+        } else {
+          dict[user.userId] = 1;
+        }
+      }
+    }
+
+    console.log(dict);
+  }
+});
