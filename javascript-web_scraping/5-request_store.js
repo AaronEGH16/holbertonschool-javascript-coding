d4eb3cbb799d4d22d5e366bb3b2092d07ea905c3
AaronEGH16 <aezegonz@gmail.com>
2024-01-23 20:07:44 -0300

    semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const request = require("request");
+const fs = require('fs');
+const request = require('request');
 const args = process.argv;
 
-request.get(`${args[2]}`, "", (err, res, body) => {
+request.get(`${args[2]}`, '', (err, res, body) => {
   if (err) {
     console.error(err);
   } else {
-    fs.writeFile(args[3], body, "utf-8", (err) => {
+    fs.writeFile(args[3], body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
       }
