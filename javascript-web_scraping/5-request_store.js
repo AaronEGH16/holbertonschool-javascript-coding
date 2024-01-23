06da30d7652ecb6535c24f449c4929a95d98fe9a
AaronEGH16 <aezegonz@gmail.com>
2024-01-23 20:05:43 -0300

    read URL content and save it into a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const request = require("request");
+const args = process.argv;
+
+request.get(`${args[2]}`, "", (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(args[3], body, "utf-8", (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
