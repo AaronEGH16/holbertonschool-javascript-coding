4e5b2f9620e3cda3522c6dbc34858b2eb0d8e406
AaronEGH16 <aezegonz@gmail.com>
2024-01-23 19:05:47 -0300

    get an array of jsons and filter results

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+request.get(`${args[2]}`, '', (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const films = JSON.parse(body);
+    const amount = films.results.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+    console.log(amount.length);
+  }
+});
