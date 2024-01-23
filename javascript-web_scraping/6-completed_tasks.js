c2869a1f6ce590d6afe2fa2bf5dfcf47562eae53
AaronEGH16 <aezegonz@gmail.com>
2024-01-23 20:33:12 -0300

    semistandard validation

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const args = process.argv;
 
 request.get(args[2], '', (err, res, body) => {
   if (err) {
     console.error(err);
   } else {
-    let dict = {};
+    const dict = {};
     const users = JSON.parse(body);
 
     for (const user of users) {
