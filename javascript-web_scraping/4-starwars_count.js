1537abae5cf6869bfc03b84663ed5e7f752ee547
AaronEGH16 <aezegonz@gmail.com>
2024-01-28 19:01:43 -0300

    added error code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,11 +5,11 @@ const args = process.argv;
 
 request.get(`${args[2]}`, '', (err, res, body) => {
   if (err) {
-    console.error(err);
+    console.error(err, 10);
     return;
   }
 
-  const films = JSON.parse(body);
-  const amount = films.results.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  const films = JSON.parse(body).results;
+  const amount = films.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
   console.log(amount.length);
 });
