19aa2ad8550c5ae6f3ccbb3770e73769c05d0274
AaronEGH16 <aezegonz@gmail.com>
2024-01-28 19:18:30 -0300

    finally i surrender with this exercise

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,11 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, '', (err, res, body) => {
-  if (!err) {
-    const films = JSON.parse(body).results;
-    const amount = films.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
-    console.log(amount.length);
+  if (err) {
+    console.error(err);
   }
+
+  const films = JSON.parse(body).results;
+  const amount = films.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  console.log(amount.length);
 });
