348ad49b928d3c9f1df8397f383384a97176edbc
AaronEGH16 <aezegonz@gmail.com>
2024-02-03 21:17:06 -0300

    removed exit after insert valid name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   const name = input.toString();
   if (name !== '\n') {
-    process.stdout.write(`Your name is: ${name}\r`);
-    process.stdin.push(null);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
