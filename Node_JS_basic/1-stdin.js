392058f2c9808d764a6a9ebb0d5ecf36534ef4f9
AaronEGH16 <aezegonz@gmail.com>
2024-02-03 21:11:36 -0300

    fixed linter

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   const name = input.toString();
-  if (name !== "\n") {
+  if (name !== '\n') {
     process.stdout.write(`Your name is: ${name}`);
     process.stdin.push(null);
   }
